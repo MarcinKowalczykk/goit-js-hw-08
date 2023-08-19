e5e131639a2ecb85056f03c46cbd75f6049dc31e
Marcin Kowalczyk <marcinck80@gmail.com>
2023-08-19 16:58:52 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const galleryImages = galleryItems
+  .map(
+    img =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+  </li>`
+  )
+  .join('');
+
+gallery.innerHTML = galleryImages;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
+
+
