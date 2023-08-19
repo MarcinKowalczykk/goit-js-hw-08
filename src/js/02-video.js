003a20a2c1319b0a70743cbf2f989d96371a6efb
Marcin Kowalczyk <marcinck80@gmail.com>
2023-08-19 17:39:37 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const vimeoPlayer = new Vimeo('vimeo-player', {
+  id: 236203659,
+});
 
+const resumeVideoFromLocalStorage = async () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime !== null) {
+    await vimeoPlayer.setCurrentTime(parseFloat(currentTime));
+  }
+};
+
+vimeoPlayer.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  resumeVideoFromLocalStorage();
+});
